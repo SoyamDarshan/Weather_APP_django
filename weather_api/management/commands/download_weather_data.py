2f1eb6f7e4a240e358c9e863fe577559d006306f
SoyamDarshan <soyam.darshan@gmail.com>
2019-03-14 01:35:56 +0530

    download_weather_data --updated

--- weather_api/management/commands/download_weather_data.py
+++ weather_api/management/commands/download_weather_data.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen, Request
+from urllib.error import HTTPError, URLError
 import json
 import datetime
 from django.core.management.base import BaseCommand
@@ -13,8 +14,6 @@ class Command(BaseCommand):
     def handle(self, **options):
         country_names = ["UK", "England", "Scotland", "Wales"]
         metrics = ["Tmax", "Tmin", "Rainfall"]
-        #country_names = ["UK"]
-        #metrics = ["Tmax"]
         download_path = "weather_api/fixtures"
 
         def myconverter(o):
@@ -24,20 +23,25 @@ class Command(BaseCommand):
         for country in country_names:
             for metric in metrics:
                 filename = "{0}_{1}.json".format(metric, country)
-                req = Request("https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/{0}-{1}.json".format(metric, country))
-                print("Request")
-                with urlopen(req) as response:
-                    print("urlopen")
-                    with open("{0}/{1}".format(download_path, filename), "w+") as outputfile:
-                        result = json.loads(response.read().decode("utf-8"))
-                        model_name = 'weather_api.{0}data'.format(metric)
-                        load_data = []
-                        for i in range(len(result)):
-                            result[i]["country_name"] = "{0}".format(country)
-                            #print(result[i])
-                            result[i]["date_val"] = myconverter(datetime.date(result[i]["year"], result[i]["month"], 1))
-                            print(result[i])
-                            load_data.append({"model": model_name, "fields": result[i]})
-                        #print(load_data)
-                        json.dump(load_data, outputfile)
-                print("{0} has been created in {1} directory.".format(filename, download_path))
+                try:
+                    req = Request("https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/{0}-{1}.json".format(metric, country))
+                    print("Request")
+                    with urlopen(req) as response:
+                        print("urlopen")
+                        with open("{0}/{1}".format(download_path, filename), "w+") as outputfile:
+                            result = json.loads(response.read().decode("utf-8"))
+                            model_name = 'weather_api.{0}data'.format(metric)
+                            load_data = []
+                            for i in range(len(result)):
+                                result[i]["country_name"] = "{0}".format(country)
+                                # print(result[i])
+                                result[i]["date_val"] = myconverter(datetime.date(result[i]["year"], result[i]["month"], 1))
+                                print(result[i])
+                                load_data.append({"model": model_name, "fields": result[i]})
+                            # print(load_data)
+                            json.dump(load_data, outputfile)
+                    print("{0} has been created in {1} directory.".format(filename, download_path))
+                except HTTPError as e:
+                    print(e.code)
+                except URLError as e:
+                    print(e.code)
